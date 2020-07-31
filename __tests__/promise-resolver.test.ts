c8556777ac2cdbbd0894b2c1d0c3e6b82be6cebd
Denis Olsem <dols3m@gmail.com>
2020-07-30 19:01:40 -0700

    test: fix promise-resolver test

--- __tests__/promise-resolver.test.ts
+++ __tests__/promise-resolver.test.ts
@@ -1,7 +1,10 @@
 import { createPromiseResolver } from '../src/promise-resolver';
+import type { ThunklessAction } from '../src/thunkless-action.interface';
+import { Dispatch, AnyAction } from 'redux';
 
 const store = { dispatch: jest.fn(), getState: jest.fn() };
-const next = jest.fn();
+const mockValue = Symbol();
+const next = jest.fn((action: ThunklessAction<any>) => mockValue);
 
 const resolve = createPromiseResolver(store);
 
@@ -11,24 +14,22 @@ afterEach(() => {
   next.mockClear();
 });
 
-// it('resolves promises', () => {
-//   const payload = Symbol();
-//   const successType = 'SUCCESS_TYPE';
+it('resolves promises', async () => {
+  const payload = Symbol();
+  const successType = 'SUCCESS_TYPE';
 
-//   const promise = resolve(next, Promise.resolve(payload), null, successType, null, null, null, null);
-//   expect(promise).resolves.toBe(payload);
-//   return promise.then(() => {
-//     expect(next).toHaveBeenCalled();
-//     expect(next.mock.calls[0][0]).toHaveProperty('payload', payload);
-//     expect(next.mock.calls[0][0]).toHaveProperty('type', successType);
-//   });
-// });
+  const promise = resolve(next as Dispatch<AnyAction>, Promise.resolve(payload), null, successType, null, null, null, null);
+  await expect(promise).resolves.toBe(mockValue);
+  expect(next).toHaveBeenCalled();
+  expect(next.mock.calls[0][0]).toHaveProperty('payload', payload);
+  expect(next.mock.calls[0][0]).toHaveProperty('type', successType);
+});
 
 describe('chain actions', () => {
   it('dispatches single action', async () => {
     const chain = { type: 'TYPE_ONE' };
   
-    await resolve(next, Promise.resolve(), null, null, null, chain, null, null);
+    await resolve(next as Dispatch<AnyAction>, Promise.resolve(), null, null, null, chain, null, null);
     expect(store.dispatch).toHaveBeenCalled();
     expect(store.dispatch.mock.calls[0][0]).toEqual(chain);
   });
@@ -36,7 +37,7 @@ describe('chain actions', () => {
   it('dispatches multiple actions', async () => {
     const chain = [{ type: 'TYPE_ONE' }, { type: 'TYPE_TWO' }];
 
-    await resolve(next, Promise.resolve(), null, null, null, chain.concat(null), null, null);
+    await resolve(next as Dispatch<AnyAction>, Promise.resolve(), null, null, null, chain.concat(null), null, null);
     expect(store.dispatch).toHaveBeenCalledTimes(2);
     store.dispatch.mock.calls.forEach(([action], index) => {
       expect(action).toEqual(chain[index]);
@@ -46,7 +47,7 @@ describe('chain actions', () => {
   it('supports promises', async () => {
     const chain = [{ type: 'TYPE_ONE' }, { type: 'TYPE_TWO' }];
 
-    await resolve(next, Promise.resolve(), null, null, null, Promise.resolve(chain), null, null);
+    await resolve(next as Dispatch<AnyAction>, Promise.resolve(), null, null, null, Promise.resolve(chain), null, null);
     expect(store.dispatch).toHaveBeenCalledTimes(2);
     store.dispatch.mock.calls.forEach(([action], index) => {
       expect(action).toEqual(chain[index]);
@@ -58,7 +59,7 @@ describe('chain actions', () => {
     const chain = payload => actionTypes.map(type => ({ type, payload }));
     const payload = Symbol();
 
-    await resolve(next, Promise.resolve(payload), null, null, null, chain, null, null);
+    await resolve(next as Dispatch<AnyAction>, Promise.resolve(payload), null, null, null, chain, null, null);
     expect(store.dispatch).toHaveBeenCalledTimes(2);
     store.dispatch.mock.calls.forEach(([action], index) => {
       expect(action).toHaveProperty('type', actionTypes[index]);
@@ -71,7 +72,7 @@ describe('chain actions', () => {
     const chain = async (payload) => actionTypes.map(type => ({ type, payload }));
     const payload = Symbol();
 
-    await resolve(next, Promise.resolve(payload), null, null, null, chain, null, null);
+    await resolve(next as Dispatch<AnyAction>, Promise.resolve(payload), null, null, null, chain, null, null);
     expect(store.dispatch).toHaveBeenCalledTimes(2);
     store.dispatch.mock.calls.forEach(([action], index) => {
       expect(action).toHaveProperty('type', actionTypes[index]);
@@ -84,7 +85,7 @@ it('supports actions with extra props', async () => {
   const extraFields = { meta: Symbol(), otherProp: Symbol() };
   const successType = 'SUCCESS_TYPE';
 
-  await resolve(next, Promise.resolve(), null, successType, null, null, null, extraFields);
+  await resolve(next as Dispatch<AnyAction>, Promise.resolve(), null, successType, null, null, null, extraFields);
   const [[errorAction]] = next.mock.calls;
   expect(errorAction).toHaveProperty('type', successType);
   expect(errorAction).toHaveProperty('meta', extraFields.meta);
@@ -99,7 +100,7 @@ it('handles errors', async () => {
   const extra = { prop: Symbol() };
   const failureType = 'FAILURE_TYPE';
 
-  await resolve(next, promise, action, null, failureType, chainOne, null, extra);
+  await resolve(next as Dispatch<AnyAction>, promise, action, null, failureType, chainOne, null, extra);
   expect(next).toHaveBeenCalledTimes(1);
   
   const [[failureAction]] = next.mock.calls;
@@ -109,7 +110,7 @@ it('handles errors', async () => {
   expect(failureAction).toHaveProperty('prop', extra.prop);
   expect(failureAction.payload.error).toBeInstanceOf(TypeError);
 
-  await resolve(next, Promise.resolve(), action, null, failureType, chainTwo, null, null);
+  await resolve(next as Dispatch<AnyAction>, Promise.resolve(), action, null, failureType, chainTwo, null, null);
   expect(next).toHaveBeenCalledTimes(2);
   expect(next.mock.calls[1][0].payload.error).toBeInstanceOf(TypeError);  
 });
@@ -123,11 +124,11 @@ it('supports dispatchOnError', async () => {
   const dispatchOnErrorString = errorActionType;
   const dispatchOnErrorFunc = payload => ({ type: errorActionType, error: true, payload });
 
-  await resolve(next, promise, action, null, failureType, null, dispatchOnErrorString, null);
+  await resolve(next as Dispatch<AnyAction>, promise, action, null, failureType, null, dispatchOnErrorString, null);
   expect(next).toHaveBeenCalledTimes(1);
   expect(store.dispatch).toHaveBeenCalledTimes(1);
 
-  await resolve(next, promise, action, null, failureType, null, dispatchOnErrorFunc, null);
+  await resolve(next as Dispatch<AnyAction>, promise, action, null, failureType, null, dispatchOnErrorFunc, null);
   expect(next).toHaveBeenCalledTimes(2);
   expect(store.dispatch).toHaveBeenCalledTimes(2);
 
@@ -143,7 +144,7 @@ it('supports dispatchOnError', async () => {
   expect(errorActionTwo).toHaveProperty('payload.origin', action);
   expect(errorActionTwo.payload.error).toBeInstanceOf(TypeError);
 
-  await resolve(next, promise, action, null, failureType, null, () => null, null);
+  await resolve(next as Dispatch<AnyAction>, promise, action, null, failureType, null, () => null, null);
   expect(next).toHaveBeenCalledTimes(3);
   expect(store.dispatch).toHaveBeenCalledTimes(2);
 });
